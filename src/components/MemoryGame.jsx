aa93a3a87e6533af6e2713e056c0439b2edd2353
saloni-gupta27 <guptasaloni.2016@gmail.com>
2025-08-29 16:23:11 +0530

    adding move counter

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -3,17 +3,20 @@ import shuffle from "lodash-es/shuffle";
 
 const MemoryGame = ({ images }) => {
   const gameImages = useMemo(() => shuffle([...images, ...images]), [images]);
-
-  const [openedImages, setOpenedImages] = useState([]);  // [3,4]
+ console.log(gameImages)
+  const [openedImages, setOpenedImages] = useState([]); // [3,4]
   const [matchedImages, setMatchedImages] = useState({}); // all the flipped matched cards index will be made true
   const timeoutRef = useRef(null);
-  const [gameOver,setGameOver] = useState(false)
+  const [gameOver, setGameOver] = useState(false);
+  const [counter,setCounter] = useState(0);
 
   useEffect(() => {
     if (openedImages.length !== 2) return;
 
     const [firstFlippedImgIndex, secondFlippedImgIndex] = openedImages;
-    if (gameImages[firstFlippedImgIndex] === gameImages[secondFlippedImgIndex]) {
+    if (
+      gameImages[firstFlippedImgIndex] === gameImages[secondFlippedImgIndex]
+    ) {
       // Match found
       setMatchedImages((prev) => ({
         ...prev,
@@ -21,17 +24,13 @@ const MemoryGame = ({ images }) => {
         [secondFlippedImgIndex]: true,
       }));
       setOpenedImages([]);
-      
     } else {
       // No match: flip them back after a delay
       timeoutRef.current = setTimeout(() => {
         setOpenedImages([]);
       }, 1000);
     }
-
-    
   }, [openedImages, gameImages]);
-
 
   useEffect(() => {
     // Check for end-game condition
@@ -47,28 +46,45 @@ const MemoryGame = ({ images }) => {
   }, []);
 
   const toggleFlip = (index) => {
-    if (openedImages.includes(index) || matchedImages[index] || openedImages.length === 2) {
+    if (
+      openedImages.includes(index) ||
+      matchedImages[index] ||
+      openedImages.length === 2
+    ) {
       return;
     }
     setOpenedImages((prev) => [...prev, index]);
+    setCounter(counter+1);
   };
 
   return (
     <>
-      <div className="mx-auto p-10 justify-center h-screen w-10/12 2xl:w-2/3">
-      {gameOver && (
-        <div className="absolute top-0 left-0 size-full backdrop:backdrop-blur-xl flex text-center justify-center p-3 text-3xl font-extrabold text-black bg-slate-100 opacity-90 " >
-          <div className="relative top-[45%]">
-          Congratulations! You won! 🎉 
-          <br />
-          <span onClick={()=>{location.reload()}}>Play Again!!!</span>
-          </div>
+      <div className={`mx-auto p-10 justify-center h-screen w-10/12 2xl:w-2/3" ${gameOver && "overflow-hidden" }`}>
+      <div className="">
+        <div className="delius text-center text-cyan-600 pb-3 text-3xl font-medium">Test Your Brain</div>
+        <div className="text-right prata px-5 pb-3 text-xl ">Best Score : 15</div>
+        <div className="text-right prata px-5 pb-3 text-xl ">Moves : {counter}</div>
         </div>
-      )}        <div className="h-full">
+        {gameOver && (
+          <div className="absolute top-0 left-0 size-full backdrop:backdrop-blur-xl flex text-center justify-center p-3 text-3xl font-extrabold text-black bg-slate-100 opacity-90 overflow-hidden">
+            <div className="relative top-[45%]">
+              Congratulations! You won! 🎉
+              <br />
+              <span
+                onClick={() => {
+                  location.reload();
+                }}
+              >
+                Play Again!!!
+              </span>
+            </div>
+          </div>
+        )}{" "}
+        <div className="h-full">
           <div className="flex flex-wrap gap-5 justify-center">
             {gameImages.map((image, index) => {
               const isFlipped =
-              openedImages.includes(index) || matchedImages[index];
+                openedImages.includes(index) || matchedImages[index];
               //const isFlipped = flipped[index] || false;
               return (
                 <div
@@ -77,9 +93,9 @@ const MemoryGame = ({ images }) => {
                   onClick={() => toggleFlip(index)}
                 >
                   {isFlipped ? (
-                    <img src={image} className="size-56" />
+                    <img src={image} className="card-size" />
                   ) : (
-                    <div className="size-56 bg-slate-400"></div>
+                    <div className="card-size bg-slate-400"></div>
                   )}
                 </div>
               );
